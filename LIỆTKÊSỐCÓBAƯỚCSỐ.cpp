8e9f0768e603200dee3db346aaf4c690fd3b018a
Phongtnhh <phongtnck57@gmail.com>
2024-07-08 01:41:32 +0700

    create LIỆTKÊSỐCÓBAƯỚCSỐ.cpp

---
+++ LIỆTKÊSỐCÓBAƯỚCSỐ.cpp
@@ -0,0 +1,28 @@
+#include<bits/stdc++.h>
+using namespace std;
+using ll = long long;
+vector<bool> isPrime(10e6, true);
+void prime(){
+    isPrime[0] = isPrime[1] = false; 
+    for (int i = 2; i * i <= pow(10,6); ++i) {
+        if (isPrime[i]) {
+            for (int j = i * i; j <= 10e6; j += i) {
+                isPrime[j] = false;
+            }
+        }
+    }
+
+}
+int main(){
+	int test; cin >> test; 
+	prime();
+	while(test--){
+		int n; cin >> n;
+		for(int i = 2; i <= sqrt(n);i++){
+			if(isPrime[i]){
+				cout << i*i <<" ";
+			}
+		}
+		cout << endl;
+	}
+}
